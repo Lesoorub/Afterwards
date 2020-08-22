16f28a3bfff48b8b82d788543ad646e5df74bdf7
Andrew <andrey.zuev.2001@yandex.ru>
2020-08-22 11:42:25 +0300

    Заполнил начальную страницу

--- siteProject/Main/urls.py
+++ siteProject/Main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('auth', views.auth, name='auth'),
     path('index', views.index, name='index'),
-    path('', views.index, name='home'),
-
+    path('', views.index, name='home')
 ]
--- siteProject/Main/views.py
+++ siteProject/Main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def auth(request):
+
+    return render(request, 'main/auth.html')
