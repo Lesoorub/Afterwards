3cdf19399c86218c6448b722affae13b7f57c8e4
Andrew <andrey.zuev.2001@yandex.ru>
2020-08-22 12:16:14 +0300

    Заполняйте доп. страницы

--- siteProject/Main/urls.py
+++ siteProject/Main/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('auth', views.auth, name='auth'),
     path('index', views.index, name='index'),
+    path('more-about-us', views.moreaboutus, name='more-about-us'),
+    path('TransparentVotingSystem', views.TransparentVotingSystem, name='TransparentVotingSystem'),
+    path('ConvenienceAndSimplicity', views.ConvenienceAndSimplicity, name='ConvenienceAndSimplicity'),
+    path('UniversalNotification', views.UniversalNotification, name='UniversalNotification'),
     path('', views.index, name='home')
 ]
--- siteProject/Main/views.py
+++ siteProject/Main/views.py
@@ -5,5 +5,16 @@ def index(request):
     return render(request, 'main/index.html')
 
 def auth(request):
-
     return render(request, 'main/auth.html')
+
+def moreaboutus(request):
+    return render(request, 'main/more-about-us.html')
+
+def TransparentVotingSystem(request):
+    return render(request, 'main/TransparentVotingSystem.html')
+
+def ConvenienceAndSimplicity(request):
+    return render(request, 'main/ConvenienceAndSimplicity.html')
+
+def UniversalNotification(request):
+    return render(request, 'main/UniversalNotification.html')
